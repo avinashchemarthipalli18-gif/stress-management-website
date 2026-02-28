516c695da98786737ebb652290606be9ab5d4c3a
avinashchemarthipalli18-gif <avinashchemarthipalli18@gmail.com>
2026-02-28 12:57:03 +0530

    Refactor emoji background creation and interval logic

--- emoji-background.js
+++ emoji-background.js
@@ -1,46 +1,43 @@
-// Create emoji container
+const emojis = ["😊","😌","🌿","💙","✨","☁️"];
+
 const container = document.createElement("div");
 
-container.style.position = "fixed";
-container.style.top = "0";
-container.style.left = "0";
-container.style.width = "100%";
-container.style.height = "100%";
-container.style.pointerEvents = "none";
-container.style.zIndex = "-1";
+container.style.position="fixed";
+container.style.top="0";
+container.style.left="0";
+container.style.width="100%";
+container.style.height="100%";
+container.style.pointerEvents="none";
+container.style.zIndex="-1";
 
 document.body.appendChild(container);
 
-// Emoji list
-const emojis = ["😊","😌","😇","✨","💙","🌿","☁️"];
-
-// Create emoji function
 function createEmoji(){
 
-const emoji = document.createElement("div");
+const emoji=document.createElement("div");
 
-emoji.innerHTML = emojis[Math.floor(Math.random()*emojis.length)];
+emoji.innerHTML=emojis[Math.floor(Math.random()*emojis.length)];
 
-emoji.style.position = "absolute";
-emoji.style.left = Math.random()*100 + "%";
-emoji.style.bottom = "-50px";
-emoji.style.fontSize = (20 + Math.random()*40) + "px";
-emoji.style.opacity = "0.3";
+emoji.style.position="absolute";
+emoji.style.left=Math.random()*100+"%";
+emoji.style.bottom="-50px";
+emoji.style.fontSize="30px";
+emoji.style.opacity="0.3";
 
 container.appendChild(emoji);
 
-let pos = -50;
+let pos=-50;
 
-const interval = setInterval(()=>{
+let interval=setInterval(()=>{
 
 pos++;
 
-emoji.style.bottom = pos + "px";
+emoji.style.bottom=pos+"px";
 
-if(pos > window.innerHeight){
+if(pos>window.innerHeight){
 
-emoji.remove();
 clearInterval(interval);
+emoji.remove();
 
 }
 
@@ -48,5 +45,4 @@ clearInterval(interval);
 
 }
 
-// Repeat
 setInterval(createEmoji,1000);
